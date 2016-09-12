bc15bf428f52b725f2efb633b0aa251668c6c1f1
marcoberri <marcoberri@gmail.com>
2016-09-12 02:47:14 +0200

    first import

--- microservice-ms/proxy-balancer/src/main/java/it/marcoberri/ms/proxybal/GatewayApplication.java
+++ microservice-ms/proxy-balancer/src/main/java/it/marcoberri/ms/proxybal/GatewayApplication.java
@@ -1,4 +1,4 @@
-package it.marcoberri.proxybal;
+package it.marcoberri.ms.proxybal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
