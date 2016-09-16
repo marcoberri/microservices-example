461bd21c3f6d16e8cebd9401d09a31e394a20b63
marcoberri <marcoberri@gmail.com>
2016-09-16 07:57:11 +0200

    commit fix and batch

--- microservice-ms/service-login/src/main/java/it/marcoberri/ms/service/login/restcontroller/LoginRestController.java
+++ microservice-ms/service-login/src/main/java/it/marcoberri/ms/service/login/restcontroller/LoginRestController.java
@@ -2,8 +2,11 @@ package it.marcoberri.ms.service.login.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.marcoberri.ms.service.login.model.InfoRestModel;
 
 @RestController
 public class LoginRestController {
@@ -11,12 +14,13 @@ public class LoginRestController {
 	@Autowired
 	Environment environment;
 
-	@RequestMapping(value = "/echo")
-	public String available() {
+
+	@RequestMapping(value = "/echo",produces = MediaType.APPLICATION_JSON_VALUE)
+	public InfoRestModel info() {
 
 		String port = environment.getProperty("local.server.port");
 
-		return "Spring in Action on " + System.getProperty("spring.config.name") + " on " + port;
-	}
+		return new InfoRestModel(System.getProperty("spring.config.name"), Integer.parseInt(port));
 
+	}
 }
